f55ef28c70269833ce1303e666dc9ec85c1c81aa
sergii13 <sergii.khilchuk13@gmail.com>
2023-06-30 15:48:47 +0300

    change router

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,7 @@ import RestaurantView from '@/views/RestaurantView.vue'
 import PolicyView from '@/views/PolicyView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
